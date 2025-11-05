a74b51ee742082647f952bdd9c6b692968e1f10e
sgmselli <sellings.matthew@gmail.com>
2025-11-05 17:12:29 +0000

    Fix caching error, serve from frontend container

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -25,11 +25,11 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://localhost',
+    baseURL: 'http://localhost:80/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: true
+    headless: false
   },
 
   /* Configure projects for major browsers */
